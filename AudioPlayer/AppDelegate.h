4daebdde26b5550fcfa7231fb86e08eeaf2557f7
xiaokai.zhan <zhanxiaokai@changba.com>
2017-02-10 16:10:10 +0800

    Initial Commit

---
+++ AudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioPlayer
+//
+//  Created by apple on 2017/2/10.
+//  Copyright © 2017年 xiaokai.zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioPlayer
+//
+//  Created by apple on 2017/2/10.
+//  Copyright © 2017年 xiaokai.zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
